e11f5bb130f894dda4629cfbfeac998f987a905d
Aman Tripathi <aman.tripathi@shipglobal.in>
2025-02-01 15:41:50 +0530

    Made FormInput component for FormField

---
+++ src/components/elements/FormInput.tsx
@@ -0,0 +1,27 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+export const FormInput = ({ control, name, label, className }) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className={className}>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
